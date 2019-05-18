2a8042f7eab837ea28ad80aec03da3dbe3f52a16
Jay <q522527649@126.com>
2019-05-18 15:27:54 +0800

    code

--- test.py
+++ test.py
@@ -7,9 +7,9 @@ import json
 import bert_encoder
 
 # Hyper Parameters
-EPOCH = 3
-BATCH_SIZE = 100
-LR = 1e-3
+EPOCH = 100
+BATCH_SIZE = 50
+LR = 1e-4
 
 #customized loading data
 class CustomDataset(Dataset):
@@ -22,8 +22,13 @@ class CustomDataset(Dataset):
         positive = torch.FloatTensor([1])
         negative = torch.FloatTensor([0])
         for passage in passages:
-            self.data.append(torch.FloatTensor(be.encode(passage["passage"])).squeeze(0).transpose(0, 1))
-            self.label.append(positive)
+            while len(passage["passage"]) > 32: #abandon too short section
+                self.data.append(torch.FloatTensor(be.encode(passage["passage"][:128])).squeeze(0).transpose(0, 1))
+                if passage["label"] == 1:
+                    self.label.append(positive)
+                else:
+                    self.label.append(negative)
+                passage["passage"] = passage["passage"][128:]
         inp.close()
 
     def __getitem__(self, index):
@@ -32,8 +37,8 @@ class CustomDataset(Dataset):
     def __len__(self):
         return len(self.data)
 
-train_loader = Data.DataLoader(dataset = CustomDataset("data.json"), batch_size = BATCH_SIZE, shuffle = True)
-#test_loader = Data.DataLoader(dataset = test_data, batch_size = BATCH_SIZE, shuffle = True)
+train_loader = Data.DataLoader(dataset = CustomDataset("train.json"), batch_size = BATCH_SIZE, shuffle = True)
+test_loader = Data.DataLoader(dataset = CustomDataset("test.json"), batch_size = BATCH_SIZE, shuffle = True)
 
 #model
 class CNN(nn.Module):
@@ -55,19 +60,14 @@ class CNN(nn.Module):
             nn.Linear(128 * 16, 128),
         	nn.ReLU(),
         	nn.Dropout(),
-        	nn.Linear(128, out_class)
+        	nn.Linear(128, out_class),
+            nn.Sigmoid()
         )
     def forward(self, x):
         out = self.conv(x)
         out = out.view(out.size(0), -1) #unfold
         out = self.fc(out)
         return out
-
-'''m = nn.Conv1d(16, 33, 3, stride=2)
-inp = torch.randn(20, 16, 50)
-output = m(inp)
-print(output.shape)
-os._exit()'''
 
 cnn = CNN(768, 1) #bert output a vector of 768 for every word, 
                   #and the output mental analysis is binary classification
@@ -79,26 +79,22 @@ for epoch in range(EPOCH):
     for step, data in enumerate(train_loader):
         vec, label = data
         output = cnn(vec)
-        output[output < 0] = 0
-        output[output > 1] = 1
         loss = loss_func(output, label)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
         #output process every 100 batch
-        if step % 100 == 0:
-            pred = torch.max(output, 1)[1].data.numpy() #find the largest index to have maximum
-            accuracy = float((pred == label.data.numpy()).astype(int).sum()) / float(label.size(0))
+        if step % 1 == 0:
+            output = output - label #count right answer
+            accuracy = float(output[((output >= -0.5) & (output <= 0.5))].size(0)) / float(label.size(0))
             print('Epoch:', epoch, '|| Loss:%.4f' % loss, '|| Accuracy:%.2f' % accuracy)
-    break
-'''
+
 #test
 for step, data in enumerate(test_loader):
     vec, label = data
     output = cnn(vec)
-    if step % 20 == 0:
-        test_pred = torch.max(output, 1)[1].data.numpy()
-        print('Prediction number:', test_pred, '\nReal number:', label.numpy())
-        accuracy = float((test_pred == label.data.numpy()).astype(int).sum()) / float(label.size(0))
-        print('Accuracy:%.2f' % accuracy)'''
+    if step % 1 == 0:
+        output = output - label
+        accuracy = float(output[((output >= -0.5) & (output <= 0.5))].size(0)) / float(label.size(0))
+        print('Accuracy:%.2f' % accuracy)
