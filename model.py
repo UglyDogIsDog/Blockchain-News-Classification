c84c7648bf9f1dc47df4d13f0d5e68b161c1f6a2
ZJUHSY <36228532+ZJUHSY@users.noreply.github.com>
2019-05-23 10:56:17 +0800

    'a'

--- model.py
+++ model.py
@@ -18,7 +18,7 @@ class CNN_Text(nn.Module):
 
     def forward(self, x):
         x = x.unsqueeze(1)  # (N, Ci, W, D)
-        print(x.shape)
+        #print(x.shape)
         x = [F.relu(conv(x)).squeeze(3) for conv in self.convs1]  # [(N, Co, W+-), ...]*len(Ks)
         x = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x]  # [(N, Co), ...]*len(Ks)
         x = torch.cat(x, 1)
