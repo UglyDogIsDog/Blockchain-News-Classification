68c9fa3e65b437c635e71b5b69eb7712ccab2c35
Jay <q522527649@126.com>
2019-06-23 22:33:25 +0800

    regularization

--- run.py
+++ run.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     cnn = CNN_Text()
     if use_cuda:
         cnn = cnn.cuda()
-    optimizer = torch.optim.Adam(cnn.parameters(),lr = LR)
+    optimizer = torch.optim.Adam(cnn.parameters(), lr = LR, weight_decay=0.0005)
 
     #train
     for epoch in range(EPOCH):
