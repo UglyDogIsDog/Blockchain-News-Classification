cce824ab79bd95492532b4b73bba728a1b376dad
Jay <q522527649@126.com>
2019-06-24 00:44:57 +0800

    1

--- run.py
+++ run.py
@@ -48,8 +48,8 @@ class MLP_model(nn.Module):
 class SimpleCustomBatch:
     def __init__(self, data):
         transposed_data = list(zip(*data))
-        self.inp = transposed_data[0]
-        self.tgt = transposed_data[1]
+        self.inp = torch.stack(transposed_data[0], 0)
+        self.tgt = torch.stack(transposed_data[1], 0)
 
     def pin_memory(self):
         self.inp = self.inp.pin_memory()
