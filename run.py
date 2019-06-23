ad17ca5b3ee0c1002f7c2957c13e80da234c00e9
Jay <q522527649@126.com>
2019-06-24 00:46:15 +0800

    1

--- run.py
+++ run.py
@@ -48,6 +48,7 @@ class MLP_model(nn.Module):
 class SimpleCustomBatch:
     def __init__(self, data):
         transposed_data = list(zip(*data))
+        print(transposed_data)
         self.inp = torch.stack(transposed_data[0], 0)
         self.tgt = torch.stack(transposed_data[1], 0)
 
