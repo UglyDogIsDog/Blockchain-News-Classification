e6a9230e0015035b2d82363479663fb123012287
Jay <q522527649@126.com>
2019-06-24 19:42:58 +0800

    1

--- run.py
+++ run.py
@@ -204,7 +204,7 @@ if __name__ == "__main__":
         passages = json.load(inp)
         end = len(passages)
         for begin in range(0, end, args.step):
-            if passages[begin].get("label", None) == None:
+            if passages[max(end, begin + args.step - 1)].get("label", None) == None:
                 break
         start = begin
         inp.close()
