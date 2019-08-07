159cc5c3d3bff4e22ea33237bf01afd21b01e7b6
ZJUHSY <36228532+ZJUHSY@users.noreply.github.com>
2019-08-07 16:01:30 +0800

    'n'

--- run.py
+++ run.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
     parser.add_argument("-r", "--regularization", type=float, default=0.001) #normally 0.0005
 
     # relatively loose hyperparameters
-    parser.add_argument("-e", "--epoch", type=int, default=5000)
+    parser.add_argument("-e", "--epoch", type=int, default=50)
     parser.add_argument("-bs", "--batch_size", type=int, default=32)
     parser.add_argument("-c", "--clip", type=float, default=1)
     parser.add_argument("-hl", "--hidden_layer", type=int, default=100)
