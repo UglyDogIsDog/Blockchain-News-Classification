cf0fa95438af21736352579992d0ecf7f867d931
ZJUHSY <690757034@qq.com>
2019-10-02 00:34:01 +0800

    'n'

--- run.py
+++ run.py
@@ -87,15 +87,15 @@ if __name__ == "__main__":
     parser.add_argument("-r", "--regularization", type=float, default=0.003) #normally 0.0005
 
     # relatively loose hyperparameters
-    parser.add_argument("-e", "--epoch", type=int, default=10)
+    parser.add_argument("-e", "--epoch", type=int, default=50)
     parser.add_argument("-bs", "--batch_size", type=int, default=20)
     parser.add_argument("-c", "--clip", type=float, default=1)
-    parser.add_argument("-hl", "--hidden_layer", type=int, default=20)
+    parser.add_argument("-hl", "--hidden_layer", type=int, default=50)
     parser.add_argument("-de", "--decay_epoch", type=int, default=20)
-    parser.add_argument("-ct", "--check_time", type=int, default=1)
+    parser.add_argument("-ct", "--check_time", type=int, default=100)
     parser.add_argument("-sn", "--sen_num", type=int, default=40)
-    parser.add_argument("-tm", "--train_model", type=bool, default=True)
-    parser.add_argument("-s", "--step", type=int, default=28)
+    parser.add_argument("-tm", "--train_model", type=bool, default=False)
+  #  parser.add_argument("-s", "--step", type=int, default=28)
     args = parser.parse_args()
 
     #use CUDA to speed up
@@ -187,11 +187,11 @@ if __name__ == "__main__":
         true += labels[labels == 1].size(0)
 
             
-        if update_model:
-            
-            print("train: loss: {} ".format(total_loss), end = "" )
-        else:
-            print("dev: ", end="")
+#        if update_model:
+#            
+#            print("train: loss: {} ".format(total_loss), end = "")
+#        else:
+#            print("dev: ", end="")
         accuracy = float(right_num) / total_num
         
         print("accuracy: {} ".format(accuracy), end="")
